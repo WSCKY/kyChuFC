f41440a59d1c302f05583854aa6837fd937c55b5
kyChu <hello.kychu@gmail.com>
2018-01-10 18:10:25 +0800

    add LIMIT macros

--- Middlewares/FlightControl/Algorithm/Maths.h
+++ Middlewares/FlightControl/Algorithm/Maths.h
@@ -9,6 +9,10 @@
 #define ABS(x)                                   ((x) >= 0 ? (x) : -(x))
 #define SQUARE(x)                                ((x) * (x))
 
+#define LIMIT_MAX(x, max)                        (((x) > (max)) ? (max) : (x))
+#define LIMIT_MIN(x, min)                        (((x) < (min)) ? (min) : (x))
+#define LIMIT(x, min, max)                       (LIMIT_MAX(LIMIT_MIN(x, min), max))
+
 #endif /* __MATHS_H */
 
 /* ------------------------ (C) COPYRIGHT kyChu ----------- END OF FILE ----- */
