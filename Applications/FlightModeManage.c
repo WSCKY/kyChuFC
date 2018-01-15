de259d16b419c91ea5878ab157d82992987d9d2d
kyChu <hello.kychu@gmail.com>
2018-01-15 15:13:50 +0800

    add fly mode manager

---
+++ Applications/FlightModeManage.c
@@ -0,0 +1,45 @@
+#include "FlightModeManage.h"
+
+static uint8_t _init_flag = 0;
+static RF_COMMAND_DEF *pRF_CMD = {0};
+
+void FlightModeManageTask(uint8_t millis)
+{
+	if(_init_flag == 0) {
+		_init_flag = 1;
+
+		pRF_CMD = GetRFCommand();
+	}
+
+	if(pRF_CMD->RevTriState[3] == TriState_Low) { /* Mode Group 1 */
+		if(pRF_CMD->Mode == FlightMode1) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode2) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode3) {
+			AngleCtrlModeTask(millis);
+		} else {
+			AngleCtrlModeTask(millis);
+		}
+	} else if(pRF_CMD->RevTriState[3] == TriState_Mid) { /* Mode Group 2 */
+		if(pRF_CMD->Mode == FlightMode1) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode2) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode3) {
+			AngleCtrlModeTask(millis);
+		} else {
+			AngleCtrlModeTask(millis);
+		}
+	} else if(pRF_CMD->RevTriState[3] == TriState_High) { /* Mode Group 3 */
+		if(pRF_CMD->Mode == FlightMode1) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode2) {
+			AngleCtrlModeTask(millis);
+		} else if(pRF_CMD->Mode == FlightMode3) {
+			AngleCtrlModeTask(millis);
+		} else {
+			AngleCtrlModeTask(millis);
+		}
+	}
+}
---
+++ Applications/FlightModeManage.h
@@ -0,0 +1,11 @@
+#ifndef __FLIGHTMODEMANAGE_H
+#define __FLIGHTMODEMANAGE_H
+
+#include "AngleCtrlMode.h"
+
+#include "MotorControl.h"
+#include "RF_DataProcess.h"
+
+void FlightModeManageTask(uint8_t millis);
+
+#endif /* __FLIGHTMODEMANAGE_H */
