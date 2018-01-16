0430005431bf214e237e5434a4df114c0ed3aa33
kyChu <hello.kychu@gmail.com>
2018-01-16 16:30:25 +0800

    export flight mode

--- Applications/FlightModeManage.c
+++ Applications/FlightModeManage.c
@@ -3,6 +3,8 @@
 static uint8_t _init_flag = 0;
 static RF_COMMAND_DEF *pRF_CMD = {0};
 
+static FLIGHT_MODE _flight_mode = GROUP1_MODE1;
+
 void FlightModeManageTask(uint8_t millis)
 {
 	if(_init_flag == 0) {
@@ -14,32 +16,49 @@ void FlightModeManageTask(uint8_t millis)
 	if(pRF_CMD->RevTriState[3] == TriState_Low) { /* Mode Group 1 */
 		if(pRF_CMD->Mode == FlightMode1) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP1_MODE1;
 		} else if(pRF_CMD->Mode == FlightMode2) {
 			FlipCtrlModeTask(millis);
+			_flight_mode = GROUP1_MODE2;
 		} else if(pRF_CMD->Mode == FlightMode3) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP1_MODE3;
 		} else {
+			// add warning here ...
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP1_MODE1;
 		}
 	} else if(pRF_CMD->RevTriState[3] == TriState_Mid) { /* Mode Group 2 */
 		if(pRF_CMD->Mode == FlightMode1) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP2_MODE1;
 		} else if(pRF_CMD->Mode == FlightMode2) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP2_MODE2;
 		} else if(pRF_CMD->Mode == FlightMode3) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP2_MODE3;
 		} else {
+			// add warning here ...
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP2_MODE1;
 		}
 	} else if(pRF_CMD->RevTriState[3] == TriState_High) { /* Mode Group 3 */
 		if(pRF_CMD->Mode == FlightMode1) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP3_MODE1;
 		} else if(pRF_CMD->Mode == FlightMode2) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP3_MODE2;
 		} else if(pRF_CMD->Mode == FlightMode3) {
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP3_MODE3;
 		} else {
+			// add warning here ...
 			AngleCtrlModeTask(millis);
+			_flight_mode = GROUP3_MODE1;
 		}
 	}
 }
+
+inline FLIGHT_MODE GetFlightMode(void) { return _flight_mode; }
--- Applications/FlightModeManage.h
+++ Applications/FlightModeManage.h
@@ -7,6 +7,20 @@
 #include "MotorControl.h"
 #include "RF_DataProcess.h"
 
+typedef enum {
+	GROUP1_MODE1 = 0,
+	GROUP1_MODE2 = 1,
+	GROUP1_MODE3 = 2,
+	GROUP2_MODE1 = 3,
+	GROUP2_MODE2 = 4,
+	GROUP2_MODE3 = 5,
+	GROUP3_MODE1 = 6,
+	GROUP3_MODE2 = 7,
+	GROUP3_MODE3 = 8,
+} FLIGHT_MODE;
+
 void FlightModeManageTask(uint8_t millis);
+
+FLIGHT_MODE GetFlightMode(void);
 
 #endif /* __FLIGHTMODEMANAGE_H */
