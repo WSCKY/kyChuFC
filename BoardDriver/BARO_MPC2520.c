77a77ca1589d72122cc2480295bc1707af8f35a9
kyChu <hello.kychu@gmail.com>
2018-01-16 21:14:08 +0800

    add BARO sensor MPC2520 Driver(uncomplete)

---
+++ BoardDriver/BARO_MPC2520.c
@@ -0,0 +1,99 @@
+#include "BARO_MPC2520.h"
+
+static uint8_t _tx_rx_comp = 0;
+
+static MPC2520_INIT_STRUCT _mpc2520_init_structure = {0};
+
+static HAL_StatusTypeDef _BARO_Configure(void);
+static HAL_StatusTypeDef _BARO_GetCalibParam(void);
+static HAL_StatusTypeDef BARO_WriteReg(uint8_t reg, uint8_t val);
+static HAL_StatusTypeDef BARO_ReadReg(uint8_t reg, uint8_t num, uint8_t *t, uint8_t *r);
+static HAL_StatusTypeDef BARO_ReadRegUtil(uint8_t reg, uint8_t num, uint8_t *t, uint8_t *r);
+
+HAL_StatusTypeDef BARO_MPC2520_Init(void)
+{
+	HAL_StatusTypeDef ret = HAL_ERROR;
+	BARO_SPI_CS_DISABLE();
+	ret = _BARO_Configure();
+	return ret;
+}
+
+static HAL_StatusTypeDef _BARO_Configure(void)
+{
+	HAL_StatusTypeDef ret = HAL_ERROR;
+
+	if((ret = BARO_WriteReg(RESET_FLUSH_REG, 0x89)) != HAL_OK) return ret; /* FIFO flush, soft reset */
+	HAL_Delay(10);
+	if((ret = _BARO_GetCalibParam()) != HAL_OK) return ret;
+	if((ret = BARO_WriteReg(PRESSURE_CFG_REG, 0x77)) != HAL_OK) return ret; /* 128, 128 */
+	if((ret = BARO_WriteReg(TEMPERATURE_CFG_REG, 0x77)) != HAL_OK) return ret; /* 128, 128 */
+	if((ret = BARO_WriteReg(INTR_FIFO_CFG_REG, 0x0E)) != HAL_OK) return ret; /* Low, Disable, Disable, Disable, shift, shift, Enable, 4 */
+	if((ret = BARO_WriteReg(MODE_STATUS_CFG_REG, 0x07)) != HAL_OK) return ret; /* Continuous pressure and temperature measurement */
+	return ret;
+}
+
+static HAL_StatusTypeDef _BARO_GetCalibParam(void)
+{
+	HAL_StatusTypeDef ret = HAL_ERROR;
+	uint8_t _tx_buffer[19];
+	uint8_t _rx_buffer[19];
+	ret = BARO_ReadRegUtil(CALIB_COEF_REG_START, 18, _tx_buffer, _rx_buffer);
+	_mpc2520_init_structure.calib_param.c0 = ((int16_t)_rx_buffer[1] << 4) | ((int16_t)_rx_buffer[2] >> 4);
+	_mpc2520_init_structure.calib_param.c1 = ((int16_t)(_rx_buffer[2] & 0x0F) << 8) | ((int16_t)_rx_buffer[3]);
+	_mpc2520_init_structure.calib_param.c00 = ((int32_t)_rx_buffer[4] << 12 | (int32_t)_rx_buffer[5] << 4 | (int32_t)_rx_buffer[6] >> 4);
+	_mpc2520_init_structure.calib_param.c10 = ((int32_t)_rx_buffer[6] << 16 | (int32_t)_rx_buffer[7] << 8 | (int32_t)_rx_buffer[8]);
+	_mpc2520_init_structure.calib_param.c01 = ((int16_t)_rx_buffer[9] << 8 | (int16_t)_rx_buffer[10]);
+	_mpc2520_init_structure.calib_param.c11 = ((int16_t)_rx_buffer[11] << 8 | (int16_t)_rx_buffer[12]);
+	_mpc2520_init_structure.calib_param.c20 = ((int16_t)_rx_buffer[13] << 8 | (int16_t)_rx_buffer[14]);
+	_mpc2520_init_structure.calib_param.c21 = ((int16_t)_rx_buffer[15] << 8 | (int16_t)_rx_buffer[16]);
+	_mpc2520_init_structure.calib_param.c30 = ((int16_t)_rx_buffer[17] << 8 | (int16_t)_rx_buffer[18]);
+	return ret;
+}
+
+static HAL_StatusTypeDef BARO_WriteReg(uint8_t reg, uint8_t val)
+{
+	HAL_StatusTypeDef ret = HAL_ERROR;
+	uint8_t _tx_buffer[2];
+	uint8_t _rx_buffer[2];
+	IMU_SPI_CS_ENABLE();
+	_tx_rx_comp = 0;
+	_tx_buffer[0] = reg;
+	_tx_buffer[1] = val;
+	ret = BARO_TxRxData((uint8_t *)_tx_buffer, (uint8_t *)_rx_buffer, 2);
+	if(ret == HAL_OK) {
+		while (_tx_rx_comp == 0);
+	}
+	return ret;
+}
+
+static HAL_StatusTypeDef BARO_ReadReg(uint8_t reg, uint8_t num, uint8_t *t, uint8_t *r)
+{
+	IMU_SPI_CS_ENABLE();
+	_tx_rx_comp = 0;
+	t[0] = reg | 0x80;
+	return BARO_TxRxData((uint8_t *)t, (uint8_t *)r, num + 1); //ignore the first byte.
+}
+
+static HAL_StatusTypeDef BARO_ReadRegUtil(uint8_t reg, uint8_t num, uint8_t *t, uint8_t *r)
+{
+	HAL_StatusTypeDef ret = HAL_ERROR;
+	IMU_SPI_CS_ENABLE();
+	_tx_rx_comp = 0;
+	t[0] = reg | 0x80;
+	ret = BARO_TxRxData((uint8_t *)t, (uint8_t *)r, num + 1); //ignore the first byte.
+	if(ret == HAL_OK) {
+		while (_tx_rx_comp == 0);
+	}
+	return ret;
+}
+
+void BARO_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
+{
+	_tx_rx_comp = 1;
+	BARO_SPI_CS_DISABLE();
+}
+
+void BARO_TxRxErrorCallback(SPI_HandleTypeDef *hspi)
+{
+
+}
---
+++ BoardDriver/BARO_MPC2520.h
@@ -0,0 +1,39 @@
+#ifndef __BARO_MPC2520_H
+#define __BARO_MPC2520_H
+
+#include "BoardDefine.h"
+
+/* ----- Register Map ----- */
+#define PRESSURE_MSB_REG                         0x00 /* r */
+#define PRESSURE_LSB_REG                         0x01 /* r */
+#define PRESSURE_XLSB_REG                        0x02 /* r */
+#define TEMPERATURE_MSB_REG                      0x03 /* r */
+#define TEMPERATURE_LSB_REG                      0x04 /* r */
+#define TEMPERATURE_XLSB_REG                     0x05 /* r */
+#define PRESSURE_CFG_REG                         0x06 /* rw */
+#define TEMPERATURE_CFG_REG                      0x07 /* rw */
+#define MODE_STATUS_CFG_REG                      0x08 /* r/rw */
+#define INTR_FIFO_CFG_REG                        0x09 /* rw */
+#define INTR_STATUS_REG                          0x0A /* r */
+#define FIFO_STATUS_REG                          0x0B /* r */
+#define RESET_FLUSH_REG                          0x0C /* w */
+#define PRODUCT_REV_ID_REG                       0x0D /* r */
+#define CALIB_COEF_REG_START                     0x10
+#define CALIB_COEF_REG_END                       0x21
+
+#define PRESSURE_AT_SEA_LEVEL                    (1013.25f)
+
+typedef struct {
+	int16_t c0, c1, c01, c11, c20, c21, c30;
+	int32_t c00, c10;
+} MPC2520_CALIB_PARAM;
+
+typedef struct {
+	MPC2520_CALIB_PARAM calib_param;
+	int32_t pressure_raw;
+	int32_t temperature_raw;
+	int32_t kP;
+	int32_t kT;
+} MPC2520_INIT_STRUCT;
+
+#endif /* __BARO_MPC2520_H */
