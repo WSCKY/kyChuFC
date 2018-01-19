3744d5673a8fc5caef353d96aec54882479668a4
kyChu <hello.kychu@gmail.com>
2018-01-19 18:55:41 +0800

    add system time counter module

---
+++ Middlewares/FlightControl/TimeCounter/SystemTimeCounter.h
@@ -0,0 +1,10 @@
+#ifndef __SYSTEMTIMECOUNTER_H
+#define __SYSTEMTIMECOUNTER_H
+
+#include "BoardDefine.h"
+
+uint32_t _Get_Micros(void);
+uint32_t _Get_Millis(void);
+uint32_t _Get_Seconds(void);
+
+#endif /* __SYSTEMTIMECOUNTER_H */
